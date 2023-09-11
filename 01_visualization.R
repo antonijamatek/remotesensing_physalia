481ba5d2d68ce6eff830ba2d6c0fea4655d184e5
antonijamatek <144710084+antonijamatek@users.noreply.github.com>
2023-09-11 18:25:17 +0200

    01_visualization.R

---
+++ 01_visualization.R
@@ -0,0 +1,23 @@
+# This is a script to visualize satellite data 
+
+# This is an operation
+10 + 10
+
+# This is an objext
+c <- 10 + 10 
+c
+
+# Objects
+a <- 5 +3
+c+a
+
+# Arrays - series of numbers
+diameter <- c(100, 10, 50, 20, 15) #data of tree radius in different sampling units of a sampling field
+iron_conc. <- c(10, 1000, 20, 700, 900) #amount of iron in the soil 
+
+#Plot two arrays - a function, and inside function are arguments 
+
+plot(iron, diameter)
+
+#pch - symbol 
+#cex - size of points 
