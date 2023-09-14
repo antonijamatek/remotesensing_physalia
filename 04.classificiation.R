7623e6f6e064e4a9cb0807272c4bd6ae3b44fbc9
antonijamatek <144710084+antonijamatek@users.noreply.github.com>
2023-09-14 16:18:13 +0200

    Update 04.classificiation.R

--- 04.classificiation.R
+++ 04.classificiation.R
@@ -1,1 +1,18 @@
+#Procedure for classifying via remote sensing
 
+library(terra)
+library(imageRy)
+library(ggplot)
+
+im.list()
+
+m1992 <- im.import("matogrosso_l5_1992219_lrg.jpg")
+m2006 <- im.import("matogrosso_ast_2006209_lrg.jpg")
+
+im.plotRGB.user(m1992, 1, 2, 3)
+
+#1. training sets
+val1992  <- values (m1992)
+val1992
+
+#from these values the algorithm will get the centroids to run the analysis
