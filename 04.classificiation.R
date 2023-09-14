5957be72ff9857bc02e29f7b64689222b492e743
antonijamatek <144710084+antonijamatek@users.noreply.github.com>
2023-09-14 16:33:21 +0200

    Update 04.classificiation.R

--- 04.classificiation.R
+++ 04.classificiation.R
@@ -16,3 +16,21 @@ val1992  <- values (m1992)
 val1992
 
 #from these values the algorithm will get the centroids to run the analysis
+
+#1. training sets
+val1992  <- values (m1992)
+val1992
+
+#from these values the algorithm will get the centroids to run the analysis
+
+#2. k-means
+
+k1992 <- kmeans(val1992, centers=2)
+k1992  #for each pixel classifier gived a certain class
+
+k1992$cluster
+k1992$centers
+
+#3. the cluster values at the top of the original image (building final map)
+m1992c <- setValues(m1992[[1]], k1992$cluster) #assign new values to a raster image
+plot(m1992c)
